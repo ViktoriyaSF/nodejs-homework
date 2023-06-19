2ed2bebbc08cadb9f15d9fc76c690549ecee9187
ViktoriyaSF <kievstar01051985@gmail.com>
2023-06-19 22:10:45 +0300

    package.json

---
+++ contacts.js
@@ -0,0 +1,11 @@
+const fs = require("fs").promises;
+const contactsPath = path.join(__dirname, "contacts.json");
+
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+    
+}
+
+module.exports{ 
+    listContacts(),
+};
